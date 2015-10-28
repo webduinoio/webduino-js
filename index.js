cb6591c951ea16bee6e1cecb65b1d8215e868bca
Ke, Mingze <mingze.ke@gmail.com>
2015-10-28 16:08:44 +0800

    Node: require serialport module only in development env

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ require('./src/core/util')(webduino);
 require('./src/util/promisify')(webduino);
 require('./src/core/Transport')(webduino);
 require('./src/transport/NodeMqttTransport')(webduino);
-require('./src/transport/NodeSerialTransport')(webduino);
+if (process.env.NODE_ENV === 'development') {
+  require('./src/transport/NodeSerialTransport')(webduino);
+}
 require('./src/core/Pin')(webduino);
 require('./src/core/Module')(webduino);
 require('./src/core/Board')(webduino);
