196cbceaacbe6350cc3eee884353b922ff69f763
osk2 <sc60714@gmail.com>
2016-12-21 14:27:01 +0800

    Polish comment

--- src/module/ADXL345.js
+++ src/module/ADXL345.js
@@ -27,7 +27,7 @@
    * @namespace webduino.module
    * @class ADXL345
    * @constructor
-   * @param {webduino.Board} board The board the ADXL345 accelerometer is attached to.
+   * @param {webduino.Board} board The board that the ADXL345 accelerometer is attached to.
    * @extends {webduino.Module}
    */
   function ADXL345(board) {
