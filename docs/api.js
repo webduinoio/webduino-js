af4f625ef030710f3b79cf405143ec224484b008
Ke, Mingze <mingze.ke@gmail.com>
2018-01-15 17:13:29 +0800

    Add dist and docs folders

---
+++ docs/api.js
@@ -0,0 +1,30 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "webduino.Board",
+        "webduino.EventEmitter",
+        "webduino.Module",
+        "webduino.Transport",
+        "webduino.module.ADXL345",
+        "webduino.module.Barcode",
+        "webduino.module.Button",
+        "webduino.module.Buzzer",
+        "webduino.module.Dht",
+        "webduino.module.HX711",
+        "webduino.module.IRLed",
+        "webduino.module.IRRecv",
+        "webduino.module.Led",
+        "webduino.module.Max7219",
+        "webduino.module.Photocell",
+        "webduino.module.RFID",
+        "webduino.module.RGBLed",
+        "webduino.module.Soil",
+        "webduino.module.Ultrasonic",
+        "webduino.transport.MqttTransport",
+        "webduino.transport.NodeMqttTransport"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
